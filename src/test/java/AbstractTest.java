aa2f7b2c3b4ffadf8cd3294a691e6a6f9af440c8
Shalgenbaev <t970399@yandex.ru>
2025-03-29 00:58:57 +0500

    add allure report

--- src/test/java/AbstractTest.java
+++ src/test/java/AbstractTest.java
@@ -1,3 +1,5 @@
+package tests;
+
 import static config.UrlConstants.BASE_URI;
 
 import io.restassured.RestAssured;
@@ -5,13 +7,13 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import org.junit.Before;
 
-public class AbstractTest {
+public abstract class AbstractTest {
 
     @Before
     public void setUpForAllRequests() {
         RestAssured.requestSpecification = new RequestSpecBuilder()
-                .setContentType(ContentType.JSON)
-                .setBaseUri(BASE_URI)
-                .build();
+                .setContentType(ContentType.JSON) // Устанавливаем тип содержимого
+                .setBaseUri(BASE_URI)              // Устанавливаем базовый URI
+                .build();                          // Создаем спецификацию запроса
     }
 }
